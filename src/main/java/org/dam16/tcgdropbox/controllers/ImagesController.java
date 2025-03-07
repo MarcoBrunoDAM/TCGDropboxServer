db6d028c96b503d7f8240bb519ebb3c13b89144c
MarcoBrunoDAM <marcobc265@gmail.com>
2025-03-07 21:32:01 +0100

    07/03/2025

--- src/main/java/org/dam16/tcgdropbox/controllers/ImagesController.java
+++ src/main/java/org/dam16/tcgdropbox/controllers/ImagesController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,106 +25,124 @@ public class ImagesController {
     String token="sl.u.AFjxgNcFQtbGGuttIYnDwy7S85lLjcBXFWojpE_sWwZGrmWK8H1W__gVcV0z1C1JK1oY90AH4IbVTsn1xxPCo8b-nytbdfmytSRmMjD9MxrBqXSNO_3M2UYXlZhB0TgB77yGvaKBmvgi8qkGuyygg1PTM8D7vTFtxmr1JRzEpFe6nn4_-0UfX7cC6vOtT0ZU3TwLZjy5xXZpJ3pQrIJ3md1mnxsr_AK7PhJCoiP0utGSY4KnfyeoFr_L3adSPY8gHQA3i4W_YFQaG33kqb5mSgohIy2gRt8AthO1U2_D8loXVlK45hkzbWptoIpTD7IH0o7ybmJhXfzvBztvq_8BFoIjng_pqmn0xeo3EtmvWGekcjAcFVcJm8FuBUcCbo6c0smY6zPhFEvXiMP0zLesg6yNpTIvTZz_ZGWMlL8eNgzkP-A-iehWTlKY9BQFPBFLSGJUqZ7Jqrv-IBSivO-4nQM_gliqkVE_ta_wIGeEv6qeQftmtuBzinOkk9TkvJF7nIC1mOV5v4AKVObvlCh6jY9cKnrg947As704xZCfX1QajtkmI1e686Wpwrx_LROwjUIY4SF2FOzIFrtpKMwvzYB-6JFL_UgO4cuWQTHa0ibfVvbyW8SZH92sbi-04DchEkpcrkCyplV7WlWtF1wfv_aietRXhHBD8Vk1Rmc3zkHxyWdLurt27OU0bSuxCaMYPQv2AGTRJ7aNWn-XjKxv_YEzFZpLJMu6ePv1cBYpCVvpBKVtY91aG_14DEactJimAujo_B6ZG04oWMP5V8a_-lqk4ZllvwhO4wHnJiR7iN-2YxFJXHouROmrrg6v4N7PYu9ey9sH6xuwCVxAio7ds_YyTPZj42ebdMoKzsOBOXNRaIwbxw0MPnGSvaCQGVoY1g8gAk_e6jzIkQeTnrIK8mloAoUgvMNbxPfJSS3fygrx7kP2uoa_y-N2k92SlOTQRv522hMUM9_Az7vLRd8xWfoDsdrx_RNH_9vc7HBgdZhlntuzbl6bejd2e1FA2C8v8DLrodfDfe6kfrVlNZXCDmlYJvPuy8WfZTGQ4h9kcT_huGxJSQCQBIHztNJsfeL0FflhhMcfuiKUacx93LM2fGizoSZ0IBqmLAztaXiV5W-FSZdPTJlWPV6Mb-f8WxS9BJUygBK_N91qIq9-QyvxCuHsD7OI8QSVB_gfusI9ddVFdjLX-p_Q35lLBaCtfnqw5iA4Yj8Aq3LvjuK3HabaBqwTU5sAsh9c9YN4hxO521hW8DaLb-Z7PjG1_9-wCB4dopT4MaWxFMAL5NLWDzs97Qjkl7FmPFDNL4Xzls8FWKM1CRTKkyp9fNMTiSxl7oYItoihd-GZpx96zbG4Y5JS2F79USSuF90Bf8-GN17o18S9oPflssz3shd4Jo6bPlAjMq4FuoqKZTsI1Ci3uAdml8LIG-p7b6G85CBtdZa6k6z_2Q";
 
     @PostMapping("/upload/card")
-    public ResponseEntity<String> uploadImageCard(@RequestParam("file")MultipartFile file){
-        File tempFile = null;
-        String dropboxUrl = "";
-        try{
-            String fileName = file.getOriginalFilename();
-            tempFile = File.createTempFile(00+fileName, ".temp");
-            file.transferTo(tempFile);
+    public ResponseEntity<String> uploadImageCard(@RequestParam("file")MultipartFile file, @AuthenticationPrincipal UserDetails userDetails){
+        if(userDetails != null){
+            String admin = userDetails.getAuthorities().iterator().next().getAuthority();
+            if(admin.equals("ROLE_true")) {
+                File tempFile = null;
+                String dropboxUrl = "";
+                try {
+                    String fileName = file.getOriginalFilename();
+                    tempFile = File.createTempFile(00 + fileName, ".temp");
+                    file.transferTo(tempFile);
 
-            dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(),"/cards/"+fileName, token );
-            //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+fileName, token);
-            return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
+                    dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(), "/cards/" + fileName, token);
+                    //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+fileName, token);
+                    return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
 
-        }catch (InvalidAccessTokenException e) {
-            token = dropboxTokenGeneratorService.getAccessToken();
-            if(token == null){
-                return new ResponseEntity<>("El token no es valido o no se pudo renovar " + e.getMessage(), HttpStatus.UNAUTHORIZED);
-            }
-            try{
-                dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(),"/cards/"+file.getOriginalFilename(), token );
-                //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+file.getOriginalFilename(), token);
-               // dropboxUrl = "http://localhost:8080/public/download/" + fileName;
-                return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
-            }catch (Exception ex){
-                return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }catch (Exception e){
-            return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }finally {
-            if(tempFile != null && tempFile.exists()){
-                tempFile.delete();
+                } catch (InvalidAccessTokenException e) {
+                    token = dropboxTokenGeneratorService.getAccessToken();
+                    if (token == null) {
+                        return new ResponseEntity<>("El token no es valido o no se pudo renovar " + e.getMessage(), HttpStatus.UNAUTHORIZED);
+                    }
+                    try {
+                        dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(), "/cards/" + file.getOriginalFilename(), token);
+                        //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+file.getOriginalFilename(), token);
+                        // dropboxUrl = "http://localhost:8080/public/download/" + fileName;
+                        return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
+                    } catch (Exception ex) {
+                        return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+                } catch (Exception e) {
+                    return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+                } finally {
+                    if (tempFile != null && tempFile.exists()) {
+                        tempFile.delete();
+                    }
+                }
             }
         }
+        return null;
     }
     @PostMapping("/upload/collection")
-    public ResponseEntity<String> uploadImageCollection(@RequestParam("file")MultipartFile file){
-        File tempFile = null;
-        String dropboxUrl = "";
-        try{
-            String fileName = file.getOriginalFilename();
-            tempFile = File.createTempFile(00+fileName, ".temp");
-            file.transferTo(tempFile);
+    public ResponseEntity<String> uploadImageCollection(@RequestParam("file")MultipartFile file,@AuthenticationPrincipal UserDetails userDetails){
+        if(userDetails != null){
+            String admin = userDetails.getAuthorities().iterator().next().getAuthority();
+            if(admin.equals("ROLE_true")) {
+                File tempFile = null;
+                String dropboxUrl = "";
+                try {
+                    String fileName = file.getOriginalFilename();
+                    tempFile = File.createTempFile(00 + fileName, ".temp");
+                    file.transferTo(tempFile);
 
-            dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(),"/collections/"+fileName, token );
-            //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+fileName, token);
-            return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
+                    dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(), "/collections/" + fileName, token);
+                    //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+fileName, token);
+                    return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
 
-        }catch (InvalidAccessTokenException e) {
-            token = dropboxTokenGeneratorService.getAccessToken();
-            if(token == null){
-                return new ResponseEntity<>("El token no es valido o no se pudo renovar " + e.getMessage(), HttpStatus.UNAUTHORIZED);
-            }
-            try{
-                dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(),"/collections/"+file.getOriginalFilename(), token );
-                //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+file.getOriginalFilename(), token);
-                // dropboxUrl = "http://localhost:8080/public/download/" + fileName;
-                return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
-            }catch (Exception ex){
-                return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }catch (Exception e){
-            return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }finally {
-            if(tempFile != null && tempFile.exists()){
-                tempFile.delete();
+                } catch (InvalidAccessTokenException e) {
+                    token = dropboxTokenGeneratorService.getAccessToken();
+                    if (token == null) {
+                        return new ResponseEntity<>("El token no es valido o no se pudo renovar " + e.getMessage(), HttpStatus.UNAUTHORIZED);
+                    }
+                    try {
+                        dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(), "/collections/" + file.getOriginalFilename(), token);
+                        //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+file.getOriginalFilename(), token);
+                        // dropboxUrl = "http://localhost:8080/public/download/" + fileName;
+                        return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
+                    } catch (Exception ex) {
+                        return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+                } catch (Exception e) {
+                    return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+                } finally {
+                    if (tempFile != null && tempFile.exists()) {
+                        tempFile.delete();
+                    }
+                }
             }
         }
+        return null;
     }
     @PostMapping("/upload/product")
-    public ResponseEntity<String> uploadImageProduct(@RequestParam("file")MultipartFile file){
-        File tempFile = null;
-        String dropboxUrl = "";
-        try{
-            String fileName = file.getOriginalFilename();
-            tempFile = File.createTempFile(00+fileName, ".temp");
-            file.transferTo(tempFile);
+    public ResponseEntity<String> uploadImageProduct(@RequestParam("file")MultipartFile file,@AuthenticationPrincipal UserDetails userDetails){
+        if(userDetails != null){
+            String admin = userDetails.getAuthorities().iterator().next().getAuthority();
+            if(admin.equals("ROLE_true")) {
+                File tempFile = null;
+                String dropboxUrl = "";
+                try {
+                    String fileName = file.getOriginalFilename();
+                    tempFile = File.createTempFile(00 + fileName, ".temp");
+                    file.transferTo(tempFile);
 
-            dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(),"/products/"+fileName, token );
-            //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+fileName, token);
-            return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
+                    dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(), "/products/" + fileName, token);
+                    //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+fileName, token);
+                    return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
 
-        }catch (InvalidAccessTokenException e) {
-            token = dropboxTokenGeneratorService.getAccessToken();
-            if(token == null){
-                return new ResponseEntity<>("El token no es valido o no se pudo renovar " + e.getMessage(), HttpStatus.UNAUTHORIZED);
-            }
-            try{
-                dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(),"/products/"+file.getOriginalFilename(), token );
-                //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+file.getOriginalFilename(), token);
-                // dropboxUrl = "http://localhost:8080/public/download/" + fileName;
-                return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
-            }catch (Exception ex){
-                return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }catch (Exception e){
-            return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }finally {
-            if(tempFile != null && tempFile.exists()){
-                tempFile.delete();
+                } catch (InvalidAccessTokenException e) {
+                    token = dropboxTokenGeneratorService.getAccessToken();
+                    if (token == null) {
+                        return new ResponseEntity<>("El token no es valido o no se pudo renovar " + e.getMessage(), HttpStatus.UNAUTHORIZED);
+                    }
+                    try {
+                        dropboxService.uploadFileToDropbox(tempFile.getAbsolutePath(), "/products/" + file.getOriginalFilename(), token);
+                        //dropboxUrl = dropboxService.getDropboxImageUrl("/images/"+file.getOriginalFilename(), token);
+                        // dropboxUrl = "http://localhost:8080/public/download/" + fileName;
+                        return ResponseEntity.ok("Imagen subida con éxito. URL: " + dropboxUrl);
+                    } catch (Exception ex) {
+                        return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+                    }
+                } catch (Exception e) {
+                    return new ResponseEntity<>("ERROR AL SUBIR LA IMAGEN: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+                } finally {
+                    if (tempFile != null && tempFile.exists()) {
+                        tempFile.delete();
+                    }
+                }
             }
         }
+        return null;
     }
     @GetMapping("/download/card/{fileName}")
     public ResponseEntity<byte[]> downloadImageCard(@PathVariable String fileName) {
---
+++ src/main/java/org/dam16/tcgdropbox/security/JwtUtil.java
@@ -0,0 +1,43 @@
+package org.dam16.tcgdropbox.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject(); // Extrae el usuario
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            return getClaims(token) != null;
+        } catch (Exception e) {
+            return false; // Token inválido
+        }
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    // Extraer un dato específico (por ejemplo, "sub" o "rol")
+    public <T> T extraerDato(String token, Function<Claims, T> claimsResolver) {
+        return claimsResolver.apply(getClaims(token));
+    }
+
+    // Extraer el rol del token
+    public String extractRol(String token) {
+        return extraerDato(token, claims -> claims.get(String.valueOf("admin"), String.class));
+    }
+
+}
